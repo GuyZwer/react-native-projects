846a9002c27972bf567ba3d4c86668ce90c96cb4
guy.zwerdling <guy.zwerdling@gmail.com>
2017-10-04 15:39:21 +0300

    update

--- LearningApp/Components/LearningComponent.js
+++ LearningApp/Components/LearningComponent.js
@@ -42,6 +42,11 @@ export class Greeting extends Component {
 export class LotsOfGreetings extends Component {
   render() {
     return (
+      //in this class we calling to Greeting class and setting a key and value for this key
+      //becouse the Greeting class have a return with string "hello" and this.props he take what he got
+      //from Greeting which is the proprerties (in our case this is the name value)
+      //
+      //more specific the "this.props.name" prefer to key named "name", after he found the key he take the value of this key and display it
       <View style={{alignItems: 'center'}}>
         <Greeting name='Guy' />
         <Greeting name='Jaina' />
