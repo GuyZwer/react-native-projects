97d5392b23ff879a249a2fd0457b029cedfc7fbc
guy.zwerdling <guy.zwerdling@gmail.com>
2017-10-08 15:55:21 +0300

    update

---
+++ albums2/App.js
@@ -0,0 +1,15 @@
+// Import a Library to help create a Component
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+
+
+//Create a Component
+const App = () => (
+  <Text>Some Text</Text>
+  );
+
+
+
+//Render it to the device
+AppRegistry.registerComponent('albums', () => App);
